7c2df6fd102d9c53f164904189483b72b0b85111
aboodswiti <swaiti393@gmail.com>
2021-03-22 15:20:06 +0200

    write some tests for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CountriesService } from './services/countries.service';
+import { ThemeService } from './services/theme.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent],
+      providers: [HttpClient, CountriesService, ThemeService],
     }).compileComponents();
+  });
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -26,10 +34,25 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('countries');
   });
 
-  it('should render title', () => {
+  it('should render nav title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('countries app is running!');
+    expect(compiled.querySelector('.title ').textContent).toContain(
+      'Where in the world'
+    );
   });
+  it('should render nav title in a h5 tag', (() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+  expect(compiled.querySelector('h5').textContent).toContain('Where in the world');
+ }));
+ 
+ it("should use the ThemeService from the services", () => {
+  const themeService = fixture.debugElement.injector.get(ThemeService);
+  fixture.detectChanges();
+  expect(themeService).toEqual(component.themeService);
+});
+
 });
