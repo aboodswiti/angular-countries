70a1e58b4c7618f76e1cc79bb2bfe3ba50c7d5bf
aboodswiti <swaiti393@gmail.com>
2021-03-22 15:01:32 +0200

    create project Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CountriesComponent} from './countries/countries.component'
+import { CountryDetailsComponent } from './country-details/country-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+const routes: Routes = [
+  { path: 'countries', component: CountriesComponent},
+  { path: '', redirectTo: '/countries', pathMatch: 'full'},
+  { path: 'countries/:code', component: CountryDetailsComponent},
+  { path: '**', component:  NotFoundComponent },
 
-const routes: Routes = [];
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
