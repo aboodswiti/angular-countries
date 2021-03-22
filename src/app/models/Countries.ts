daf7a6fb25cfbb1a9cde4d4933d52cd792c3f6a0
aboodswiti <swaiti393@gmail.com>
2021-03-22 15:19:01 +0200

    create countries and countryDetails TYPE

---
+++ src/app/models/Countries.ts
@@ -0,0 +1,11 @@
+export type Countries = {
+  name: string;
+  alpha3Code: string;
+  region: string;
+  subregion: string;
+  flag: string;
+  capital: string;
+  population: number;
+  languages: Array<object>;
+  borders: string[];
+};
---
+++ src/app/models/CountryDetails.ts
@@ -0,0 +1,9 @@
+export type CountryDetails = {
+  name: string;
+  region: string;
+  subregion: string;
+  capital: string;
+  population: number;
+  borders: string[];
+  flag: string;
+};
