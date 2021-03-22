a92074df81e16ea4cd63891439c3159a09e7622f
aboodswiti <swaiti393@gmail.com>
2021-03-22 15:11:08 +0200

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CountriesService } from './services/countries.service';
+import { ThemeService } from './services/theme.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesComponent,
+    CountryDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ThemeService,CountriesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
