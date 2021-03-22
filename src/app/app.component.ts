0124393c955eee8758fa6e93294d8e2ada7987b6
aboodswiti <swaiti393@gmail.com>
2021-03-22 15:09:49 +0200

    use router-outlet & use themeService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './services/theme.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'countries';
+
+
+  get dark() {
+    return this.themeService.theme === 'dark';
+  }
+
+  set dark(enabled: boolean) {
+    this.themeService.theme = enabled ? 'dark' : null;
+  }
+
+  constructor(public themeService: ThemeService) {
+  }
 }
